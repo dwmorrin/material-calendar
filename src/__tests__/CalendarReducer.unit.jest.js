6977552e178168a4575e182e23829b6867933cc1
David Morrin <dm187@nyu.edu>
2020-04-17 11:09:57 -0400

    fix: simplifies reducer test

--- src/__tests__/CalendarReducer.unit.jest.js
+++ src/__tests__/CalendarReducer.unit.jest.js
@@ -1,33 +1,18 @@
 import calendarReducer from "../calendar/Reducer";
 import { CalendarAction } from "../calendar/types";
 
-const now = new Date();
-const later = new Date();
-later.setDate(later.getDate() + 1);
-const initialState = {
-  currentEvent: { id: 1 },
-  currentStart: now,
-  currentView: "resourceTimeGridDay",
-  detailIsOpen: false,
-  drawerIsOpen: false,
-  events: [],
-  loading: false,
-  locations: [],
-  pickerShowing: false,
-  ref: null,
-};
-
+const initialState = {};
 const modifiedState = {
-  currentEvent: { id: 2 },
-  currentStart: later,
-  currentView: "resourceTimeGridWeek",
+  currentEvent: true,
+  currentStart: true,
+  currentView: true,
   detailIsOpen: true,
   drawerIsOpen: true,
-  events: ["event"],
+  events: [],
   loading: true,
-  locations: ["location"],
+  locations: [],
   pickerShowing: true,
-  ref: {},
+  ref: true,
 };
 
 Object.keys(CalendarAction).forEach((action) => {
