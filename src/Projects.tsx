61f95f5fed157c70003a82c5ce0336dffafa260b
Michael Hagen <michaelhagen@nyu.edu>
2020-04-21 17:10:25 -0400

    move fake database to separate file

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -16,6 +16,7 @@ import ProgressBar from "./ProgressBar";
 import DropDown from "./DropDown";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
+import Database from "./Database.js";
 
 const useStyles = makeStyles((theme) => ({
   formControl: {
@@ -50,109 +51,9 @@ const Projects: FunctionComponent<RouteComponentProps> = () => {
   const pickerShowing = useState(false);
   const [drawerIsOpen, setDrawerIsOpen] = useState(false);
   const classes = useStyles();
-  const tempProjects = [
-    {
-      id: "Compression Project St3 Sp20",
-      parentId: "Engineering the Record",
-      title: "Compression Project St3 Sp20"
-    },
-    {
-      id: "Sound-Alike Phase 1 St3 Sp20",
-      parentId: "Engineering the Record",
-      title: "Sound-Alike Phase 1 St3 Sp20"
-    },
-    {
-      id: "Sound-Alike Phase 2 St3 Sp20",
-      parentId: "Engineering the Record",
-      title: "Sound-Alike Phase 2 St3 Sp20"
-    },
-    {
-      id: "Sound-Alike Phase 3 St3 Sp20",
-      parentId: "Engineering the Record",
-      title: "Sound-Alike Phase 3 St3 Sp20"
-    }
-  ];
-  const tempWeeks = [
-    {
-      id: "March 14 - March 22",
-      title: "March 14 - March 22"
-    },
-    {
-      id: "March 22 - March 29",
-      title: "March 22 - March 29"
-    },
-    {
-      id: "March 29 - April 5",
-      title: "March 29 - April 5"
-    }
-  ];
-  const tempLocations = [
-    {
-      id: "Studio 1",
-      parentId: "Studios",
-      title: "Studio 1"
-    },
-    {
-      id: "Studio 2",
-      parentId: "Studios",
-      title: "Studio 2"
-    },
-    {
-      id: "Studio 3",
-      parentId: "Studios",
-      title: "Studio 3"
-    },
-    {
-      id: "Studio 4",
-      parentId: "Studios",
-      title: "Studio 4"
-    },
-    {
-      id: "Edit Suite 1",
-      parentId: "Edit Suites",
-      title: "Edit Suite 1"
-    },
-    {
-      id: "Edit Suite 2",
-      parentId: "Edit Suites",
-      title: "Edit Suite 2"
-    },
-    {
-      id: "Edit Suite 3",
-      parentId: "Edit Suites",
-      title: "Edit Suite 3"
-    },
-    {
-      id: "Edit Suite 4",
-      parentId: "Edit Suites",
-      title: "Edit Suite 4"
-    },
-    {
-      id: "Edit Suite 5",
-      parentId: "Edit Suites",
-      title: "Edit Suite 5"
-    },
-    {
-      id: "Production Suite A",
-      parentId: "Studios",
-      title: "Production Suite A"
-    },
-    {
-      id: "Production Suite B",
-      parentId: "Studios",
-      title: "Production Suite B"
-    },
-    {
-      id: "Rehearsal Room 1",
-      parentId: "Rehearsal Rooms",
-      title: "Rehearsal Room 1"
-    },
-    {
-      id: "Rehearsal Room 2",
-      parentId: "Rehearsal Rooms",
-      title: "Rehearsal Room 2"
-    }
-  ];
+  const tempProjects = Database.projects;
+  const tempWeeks = Database.weeks;
+  const tempLocations = Database.locations;
 
   const toggleDrawer = () => (
     event: React.KeyboardEvent | React.MouseEvent
