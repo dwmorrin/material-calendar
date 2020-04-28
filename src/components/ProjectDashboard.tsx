985c015f7897624d1a4d1f5cebe0cff395aa3daf
David Morrin <dm187@nyu.edu>
2020-04-28 13:39:33 -0400

    changes grid to flexbox

--- src/components/ProjectDashboard.tsx
+++ src/components/ProjectDashboard.tsx
@@ -3,50 +3,31 @@ import React, {
   useEffect,
   useContext,
   useState,
+  Fragment,
 } from "react";
 import { CalendarUIProps, CalendarAction } from "../calendar/types";
 import {
   IconButton,
-  Grid,
-  Paper,
   makeStyles,
   Dialog,
   Toolbar,
   Typography,
   Button,
+  LinearProgress,
+  Paper,
 } from "@material-ui/core";
 import CloseIcon from "@material-ui/icons/Close";
-import Select from "./Select";
-import ProgressBar from "./ProgressBar";
 import { AuthContext } from "./AuthContext";
 import { makeTransition } from "./Transition";
 import UserGroup from "../user/UserGroup";
 
 const useStyles = makeStyles((theme) => ({
-  formControl: {
-    margin: theme.spacing(1),
-    minWidth: 120,
-  },
   root: {
     flexGrow: 1,
   },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-    color: "white",
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: "center",
-  },
-  grid: {
-    textAlign: "center",
-  },
-  text: {
-    paddingLeft: "10px",
-    textAlign: "left",
+  hoursBar: {
+    margin: theme.spacing(1),
+    height: 10,
   },
 }));
 
@@ -57,12 +38,24 @@ const ProjectDashboard: FunctionComponent<CalendarUIProps> = ({
   dispatch,
   state,
 }) => {
+  const classes = useStyles();
   const { user } = useContext(AuthContext);
   const [groups, setGroups] = useState(initialGroups);
   const { currentProject } = state;
+  const locations = state.locations.filter((location) =>
+    currentProject?.locationIds.includes(location.id)
+  );
+  const projectGroups = groups.filter((group) =>
+    user?.groupIds.includes(group.id)
+  );
+  const group = projectGroups.find(
+    (group) => group.projectId === currentProject?.id
+  );
 
   useEffect(() => {
-    if (!currentProject?.id) return;
+    if (!currentProject?.id) {
+      return;
+    }
     fetch(`/api/project_groups/${currentProject?.id}`)
       .then((response) => response.json())
       .then((groups) => {
@@ -74,9 +67,9 @@ const ProjectDashboard: FunctionComponent<CalendarUIProps> = ({
       .catch(console.error);
   }, [currentProject, user]);
 
-  const classes = useStyles();
   return (
     <Dialog
+      className={classes.root}
       fullScreen
       open={state.projectDashboardIsOpen}
       TransitionComponent={transition}
@@ -92,106 +85,46 @@ const ProjectDashboard: FunctionComponent<CalendarUIProps> = ({
         >
           <CloseIcon />
         </IconButton>
-        <Typography>Projects</Typography>
+        <Typography>{currentProject?.title}</Typography>
       </Toolbar>
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <Paper className={classes.paper}>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                Studio Allotment
-              </Grid>
-              <Grid item xs={5}>
-                <Select
-                  dispatch={dispatch}
-                  state={state}
-                  selectName="studios"
-                  selectId="studiosDropDown"
-                  contents={state.locations}
-                ></Select>
-              </Grid>
-              <Grid item xs={7}>
-                <Select
-                  dispatch={dispatch}
-                  state={state}
-                  selectName="groups"
-                  selectId="groupsSelect"
-                  contents={groups}
-                ></Select>
-              </Grid>
-              <Grid item xs={12}>
-                <ProgressBar
-                  left={{ title: "", value: 9, color: "#fc0303" }}
-                  right={{ title: "", value: 3, color: "#03fc1c" }}
-                />{" "}
-              </Grid>
-            </Grid>
-          </Paper>
-        </Grid>
-        <Grid item xs={12} className={classes.grid}>
-          <Paper className={classes.paper}>
-            <Grid container spacing={3}>
-              <Grid item xs={12}>
-                <b>My Group</b>
-              </Grid>
-              {/* <Grid item xs={5}>
-                <p className={classes.text}>
-                  {" "}
-                  Group Members:
-                  <br />
-                  {group.length < 1 ? undefined : (
-                    <Button
-                      size="small"
-                      variant="contained"
-                      color="inherit"
-                      disableElevation
-                    >
-                      Leave Group
-                    </Button>
-                  )}
-                </p>
-              </Grid> */}
-              <Grid item xs={7}>
-                <p className={classes.text}>
-                  {groups
-                    .filter((group) => user?.groupIds.includes(group.id))
-                    .map((group) => {
-                      return (
-                        <span key={group.id}>
-                          {group.title}
-                          <br />
-                        </span>
-                      );
-                    })}
-                  <Button
-                    size="small"
-                    variant="contained"
-                    color="inherit"
-                    disableElevation
-                  >
-                    Add User
-                  </Button>
-                </p>
-              </Grid>
-              <Grid item xs={12}>
-                <p className={classes.text}>My Hours:</p>
-              </Grid>
-              <Grid item xs={12}>
-                <ProgressBar
-                  left={{ title: "", value: 12, color: "#fc0303" }}
-                  right={{ title: "", value: 3, color: "#03fc1c" }}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <p className={classes.text}>Upcoming Sessions:</p>
-              </Grid>
-              <Grid item xs={12}>
-                <p className={classes.text}>Previous Sessions:</p>
-              </Grid>
-            </Grid>
-          </Paper>
-        </Grid>
-      </Grid>
+      <Paper
+        style={{
+          flexGrow: 1,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+        }}
+      >
+        <section>
+          <Typography variant="h5">Locations</Typography>
+          {locations.map((location) => (
+            <Fragment key={location.title}>
+              <Typography>{location.title}</Typography>
+              <LinearProgress // TODO remove hardcoded mock
+                className={classes.hoursBar}
+                variant="determinate"
+                value={40}
+              />
+            </Fragment>
+          ))}
+        </section>
+        <section>
+          <Typography variant="h5">Group</Typography>
+          <Typography variant="h6">Hours</Typography>
+          <LinearProgress // TODO remove hardcoded mock
+            className={classes.hoursBar}
+            variant="determinate"
+            value={20}
+          />
+          <Typography variant="h6">Members</Typography>
+          <Typography>{group?.title}</Typography>
+          <Button size="small" variant="contained" color="inherit">
+            Manage group
+          </Button>
+        </section>
+        <Typography>Upcoming Sessions</Typography>
+        <Typography>Previous Sessions</Typography>
+      </Paper>
     </Dialog>
   );
 };
