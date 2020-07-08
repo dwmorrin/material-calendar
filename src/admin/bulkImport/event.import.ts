d22a1614247b15e2adebcd0abebaf5595e3cbbf2
David Morrin <dm187@nyu.edu>
2020-07-08 15:11:15 -0400

    fix: event bulk import: adds headers, processing
    Adds missing JSON content type header
    Adds processing for the event start/end formatting

--- src/admin/bulkImport/event.import.ts
+++ src/admin/bulkImport/event.import.ts
@@ -1,5 +1,22 @@
 import { AdminAction } from "../types";
 import Event from "../../resources/Event";
+import { formatYYYYMMDD } from "../../utils/date";
+
+interface FlatEvent {
+  location: string | number;
+  locationId: number;
+  title: string;
+  start: string;
+  end: string;
+  reservable: boolean;
+}
+
+const processEvent = (event: FlatEvent): FlatEvent => ({
+  ...event,
+  locationId: +event.location,
+  start: formatYYYYMMDD(event.start),
+  end: formatYYYYMMDD(event.end),
+});
 
 const bulkImport = (
   dispatch: (action: { type: AdminAction; payload: {} }) => void,
@@ -7,8 +24,14 @@ const bulkImport = (
 ): void => {
   const errorHandler = (error: Error): void =>
     dispatch({ type: AdminAction.Error, payload: error });
-
-  fetch(`${Event.url}/bulk`, { method: "POST", body: JSON.stringify(events) })
+  const body = JSON.stringify((events as FlatEvent[]).map(processEvent));
+  fetch(`${Event.url}/bulk`, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body,
+  })
     .then((response) => response.json())
     .then(({ data, error }) => {
       if (!data) return errorHandler(error);
