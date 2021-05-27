6e967474f339ce4be32bffc6c5991ccefdbf25d8
David Morrin <dm187@nyu.edu>
2021-05-27 10:05:44 -0400

    fix: closes dialog after success

--- src/admin/locationHoursDialog.ts
+++ src/admin/locationHoursDialog.ts
@@ -45,69 +45,76 @@ const getRepeats = (repeats: FormValues): number[] =>
     [] as number[]
   );
 
-export const makeOnSubmit = (
-  dispatch: (action: Action) => void,
-  state: AdminState,
-  semester: Semester,
-  locationId: number
-) => (values: FormValues, actions: FormikValues): void => {
-  const start = { ...parseTimeFromDate(values.start as string), seconds: 0 };
-  const end = { ...parseTimeFromDate(values.end as string), seconds: 0 };
-  const from = values.from as {
-    start: string;
-    end: string;
-    allSemester: boolean;
-  };
+export const makeOnSubmit =
+  (
+    dispatch: (action: Action) => void,
+    state: AdminState,
+    semester: Semester,
+    locationId: number
+  ) =>
+  (values: FormValues, actions: FormikValues): void => {
+    const start = { ...parseTimeFromDate(values.start as string), seconds: 0 };
+    const end = { ...parseTimeFromDate(values.end as string), seconds: 0 };
+    const from = values.from as {
+      start: string;
+      end: string;
+      allSemester: boolean;
+    };
 
-  const makeDailyHours = (date: string): Omit<LocationHours, "id"> => ({
-    hours: hoursDifference(start, end),
-    date,
-    semesterId: semester.id,
-    locationId,
-    start: stringifyTime(start),
-    end: stringifyTime(end),
-  });
-
-  const repeats = getRepeats(values.repeat as FormValues);
-  const range = from.allSemester ? semester : from;
-  const hours = [
-    ...dateGenerator(trimTZ(range.start), trimTZ(range.end), repeats),
-  ].map(makeDailyHours);
-
-  const dispatchError = (error: Error): void =>
-    dispatch({ type: AdminAction.Error, payload: { error } });
-
-  const dispatchUpdatedLocations = ({ error, data }: ApiResponse): void => {
-    if (error) return dispatchError(error);
-    dispatch({
-      type: AdminAction.ReceivedResource,
-      payload: {
-        resources: {
-          ...state.resources,
-          [ResourceKey.Locations]: data as Location[],
-        },
-      },
-      meta: ResourceKey.Locations,
+    const makeDailyHours = (date: string): Omit<LocationHours, "id"> => ({
+      hours: hoursDifference(start, end),
+      date,
+      semesterId: semester.id,
+      locationId,
+      start: stringifyTime(start),
+      end: stringifyTime(end),
     });
-  };
 
-  const handleData = ({ error }: ApiResponse): void => {
-    if (error) return dispatchError(error);
-    fetch(`${Location.url}`)
+    const repeats = getRepeats(values.repeat as FormValues);
+    const range = from.allSemester ? semester : from;
+    const hours = [
+      ...dateGenerator(trimTZ(range.start), trimTZ(range.end), repeats),
+    ].map(makeDailyHours);
+
+    const dispatchError = (error: Error): void =>
+      dispatch({ type: AdminAction.Error, payload: { error } });
+
+    const dispatchUpdatedLocations = ({ error, data }: ApiResponse): void => {
+      if (error) return dispatchError(error);
+      dispatch({
+        type: AdminAction.ReceivedResource,
+        payload: {
+          resources: {
+            ...state.resources,
+            [ResourceKey.Locations]: data as Location[],
+          },
+        },
+        meta: ResourceKey.Locations,
+      });
+    };
+
+    const handleData = ({ error }: ApiResponse): void => {
+      if (error) return dispatchError(error);
+      fetch(`${Location.url}`)
+        .then((response) => response.json())
+        .then(dispatchUpdatedLocations)
+        .catch(dispatchError);
+    };
+
+    const cleanup = (): void => {
+      actions.setSubmitting(false);
+      dispatch({
+        type: AdminAction.CloseLocationHoursDialog,
+      });
+    };
+
+    fetch(`${Location.url}/${locationId}/hours/bulk`, {
+      method: "POST",
+      body: JSON.stringify(hours),
+      headers: { "Content-Type": "application/json" },
+    })
       .then((response) => response.json())
-      .then(dispatchUpdatedLocations)
-      .catch(dispatchError);
+      .then(handleData)
+      .catch(dispatchError)
+      .finally(cleanup);
   };
-
-  const cleanup = (): void => actions.setSubmitting(false);
-
-  fetch(`${Location.url}/${locationId}/hours/bulk`, {
-    method: "POST",
-    body: JSON.stringify(hours),
-    headers: { "Content-Type": "application/json" },
-  })
-    .then((response) => response.json())
-    .then(handleData)
-    .catch(dispatchError)
-    .finally(cleanup);
-};
