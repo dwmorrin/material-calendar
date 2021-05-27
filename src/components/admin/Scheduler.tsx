34b8b6a7d02dcca0e332ab00897ff74bfc044d66
David Morrin <dm187@nyu.edu>
2021-05-27 10:02:05 -0400

    feat: adds "how-to start" snackbar

--- src/components/admin/Scheduler.tsx
+++ src/components/admin/Scheduler.tsx
@@ -1,5 +1,4 @@
 import React, { FunctionComponent, memo, useEffect, useState } from "react";
-import { CircularProgress } from "@material-ui/core";
 import FullCalendar from "@fullcalendar/react";
 import resourceTimelinePlugin from "@fullcalendar/resource-timeline";
 import interactionPlugin from "@fullcalendar/interaction";
@@ -24,6 +23,7 @@ import {
 } from "../../admin/scheduler";
 import { AdminUIProps, AdminAction } from "../../admin/types";
 import { ResourceKey } from "../../resources/types";
+import { Snackbar } from "@material-ui/core";
 
 const Scheduler: FunctionComponent<AdminUIProps> = ({ dispatch, state }) => {
   const { schedulerLocationId: locationId, selectedSemester, ref } = state;
@@ -65,7 +65,9 @@ const Scheduler: FunctionComponent<AdminUIProps> = ({ dispatch, state }) => {
   const selectedLocationId = locationId || defaultLocationId;
 
   if (!semester?.start) {
-    return <CircularProgress />;
+    return (
+      <div>Invalid semester (no start date). Fix the selected semester.</div>
+    );
   }
 
   const location =
@@ -83,54 +85,61 @@ const Scheduler: FunctionComponent<AdminUIProps> = ({ dispatch, state }) => {
   ];
 
   return (
-    <FullCalendar
-      // RESOURCES
-      resources={resources}
-      resourceAreaHeaderContent={`${location.title} \u00B7 ${semester.title}`}
-      resourceAreaHeaderDidMount={({ el }): void => {
-        el.style.cursor = "pointer";
-        el.addEventListener("click", () =>
-          dispatch({ type: AdminAction.OpenSemesterDialog })
-        );
-      }}
-      resourceOrder="id" // TODO user preference; create an order prop
-      resourcesInitiallyExpanded={false} // TODO user preference
-      resourceLabelDidMount={({ resource: { id, title }, el }): void => {
-        el.style.cursor = "default"; //! TODO move to CSS
-        el.onclick = resourceClickHandler(id, title);
-      }}
-      // EVENTS
-      events={events}
-      eventClick={eventClick(dispatch, location)}
-      // SELECTIONS
-      selectable={true}
-      select={selectionHandler(dispatch, location)}
-      // VISIBLE DATE RANGE
-      initialDate={semester.start}
-      initialView="resourceTimelineSemester"
-      slotDuration={{ days: 1 }}
-      views={{
-        resourceTimelineSemester: {
-          type: "resourceTimeline",
-          dayCount: numberOfDays,
-        },
-      }}
-      // HEADER CONFIG
-      headerToolbar={false}
-      slotMinWidth={25}
-      slotLabelFormat={[
-        { month: "long" },
-        { weekday: "narrow" },
-        { day: "numeric" },
-      ]}
-      // ETC
-      timeZone="UTC"
-      nowIndicator={true}
-      height="auto"
-      plugins={[resourceTimelinePlugin, interactionPlugin]}
-      ref={ref}
-      schedulerLicenseKey="GPL-My-Project-Is-Open-Source"
-    />
+    <>
+      <Snackbar
+        open={!dailyHours.length}
+        message="Start by creating the daily hours for this location."
+        anchorOrigin={{ horizontal: "left", vertical: "top" }}
+      />
+      <FullCalendar
+        // RESOURCES
+        resources={resources}
+        resourceAreaHeaderContent={`${location.title} \u00B7 ${semester.title}`}
+        resourceAreaHeaderDidMount={({ el }): void => {
+          el.style.cursor = "pointer";
+          el.addEventListener("click", () =>
+            dispatch({ type: AdminAction.OpenSemesterDialog })
+          );
+        }}
+        resourceOrder="id" // TODO user preference; create an order prop
+        resourcesInitiallyExpanded={false} // TODO user preference
+        resourceLabelDidMount={({ resource: { id, title }, el }): void => {
+          el.style.cursor = "default"; //! TODO move to CSS
+          el.onclick = resourceClickHandler(id, title);
+        }}
+        // EVENTS
+        events={events}
+        eventClick={eventClick(dispatch, location)}
+        // SELECTIONS
+        selectable={true}
+        select={selectionHandler(dispatch, location)}
+        // VISIBLE DATE RANGE
+        initialDate={semester.start}
+        initialView="resourceTimelineSemester"
+        slotDuration={{ days: 1 }}
+        views={{
+          resourceTimelineSemester: {
+            type: "resourceTimeline",
+            dayCount: numberOfDays,
+          },
+        }}
+        // HEADER CONFIG
+        headerToolbar={false}
+        slotMinWidth={25}
+        slotLabelFormat={[
+          { month: "long" },
+          { weekday: "narrow" },
+          { day: "numeric" },
+        ]}
+        // ETC
+        timeZone="UTC"
+        nowIndicator={true}
+        height="auto"
+        plugins={[resourceTimelinePlugin, interactionPlugin]}
+        ref={ref}
+        schedulerLicenseKey="GPL-My-Project-Is-Open-Source"
+      />
+    </>
   );
 };
 
