f6dde6ffb513cebbce7f633989b1642ce647061a
David Morrin <dm187@nyu.edu>
2021-06-17 09:07:29 -0400

    fix: updates location hours (table changes)
    SQL table was updated

--- src/admin/locationHoursDialog.ts
+++ src/admin/locationHoursDialog.ts
@@ -13,67 +13,67 @@ import { ResourceKey } from "../resources/types";
 
 export type DailyHoursValue = { date: Date; hours: number };
 
-export const makeOnSubmit = (
-  dispatch: (action: Action) => void,
-  state: AdminState,
-  semester: Semester,
-  locationId: number
-) => (values: FormValues, actions: FormikValues): void => {
-  const { days } = values as { days: DailyHoursValue[] };
+export const makeOnSubmit =
+  (
+    dispatch: (action: Action) => void,
+    state: AdminState,
+    semester: Semester,
+    locationId: number
+  ) =>
+  (values: FormValues, actions: FormikValues): void => {
+    const { days } = values as { days: DailyHoursValue[] };
 
-  const makeDailyHours = (
-    date: string,
-    hours: number
-  ): Omit<LocationHours, "id"> => ({
-    hours,
-    date,
-    semesterId: semester.id,
-    locationId,
-  });
+    const makeDailyHours = (
+      date: string,
+      hours: string | number
+    ): Omit<LocationHours, "id"> => ({
+      hours: Number(hours),
+      date,
+    });
 
-  const dailyHours = days.map(({ date, hours }) =>
-    makeDailyHours(formatISO9075(date, { representation: "date" }), hours)
-  );
+    const dailyHours = days.map(({ date, hours }) =>
+      makeDailyHours(formatISO9075(date, { representation: "date" }), hours)
+    );
 
-  const dispatchError = (error: Error): void =>
-    dispatch({ type: AdminAction.Error, payload: { error } });
+    const dispatchError = (error: Error): void =>
+      dispatch({ type: AdminAction.Error, payload: { error } });
 
-  const dispatchUpdatedLocations = ({ error, data }: ApiResponse): void => {
-    if (error) return dispatchError(error);
-    dispatch({
-      type: AdminAction.ReceivedResource,
-      payload: {
-        resources: {
-          ...state.resources,
-          [ResourceKey.Locations]: data as Location[],
+    const dispatchUpdatedLocations = ({ error, data }: ApiResponse): void => {
+      if (error) return dispatchError(error);
+      dispatch({
+        type: AdminAction.ReceivedResource,
+        payload: {
+          resources: {
+            ...state.resources,
+            [ResourceKey.Locations]: data as Location[],
+          },
         },
-      },
-      meta: ResourceKey.Locations,
-    });
-  };
+        meta: ResourceKey.Locations,
+      });
+    };
 
-  const handleData = ({ error }: ApiResponse): void => {
-    if (error) return dispatchError(error);
-    fetch(`${Location.url}`)
+    const handleData = ({ error }: ApiResponse): void => {
+      if (error) return dispatchError(error);
+      fetch(`${Location.url}`)
+        .then((response) => response.json())
+        .then(dispatchUpdatedLocations)
+        .catch(dispatchError);
+    };
+
+    const cleanup = (): void => {
+      actions.setSubmitting(false);
+      dispatch({
+        type: AdminAction.CloseLocationHoursDialog,
+      });
+    };
+
+    fetch(`${Location.url}/${locationId}/hours/bulk`, {
+      method: "POST",
+      body: JSON.stringify(dailyHours),
+      headers: { "Content-Type": "application/json" },
+    })
       .then((response) => response.json())
-      .then(dispatchUpdatedLocations)
-      .catch(dispatchError);
+      .then(handleData)
+      .catch(dispatchError)
+      .finally(cleanup);
   };
-
-  const cleanup = (): void => {
-    actions.setSubmitting(false);
-    dispatch({
-      type: AdminAction.CloseLocationHoursDialog,
-    });
-  };
-
-  fetch(`${Location.url}/${locationId}/hours/bulk`, {
-    method: "POST",
-    body: JSON.stringify(dailyHours),
-    headers: { "Content-Type": "application/json" },
-  })
-    .then((response) => response.json())
-    .then(handleData)
-    .catch(dispatchError)
-    .finally(cleanup);
-};
