0072b41bf8d75d23a0f74c8928e1467cfb0d0d9c
David Morrin <dm187@nyu.edu>
2021-06-29 16:30:01 -0400

    feat: adds Paper to resource form

--- src/components/admin/forms/ResourceForm.tsx
+++ src/components/admin/forms/ResourceForm.tsx
@@ -5,7 +5,7 @@ import {
   FormTemplateProps,
   AdminState,
 } from "../../../admin/types";
-import { Box } from "@material-ui/core";
+import { Box, DialogContent, Paper } from "@material-ui/core";
 import SaveButton from "./SaveButton";
 import DeleteButton from "./DeleteButton";
 
@@ -17,31 +17,37 @@ interface ResourceFormProps {
 const ResourceForm: FunctionComponent<
   FormikProps<FormValues> & ResourceFormProps
 > = ({ FormFields, handleSubmit, setFieldValue, values, state }) => (
-  <Form onSubmit={handleSubmit}>
-    <Box style={{ marginLeft: 10, display: "flex", flexDirection: "column" }}>
-      <h2>
-        Warning: this interface under construction and may not write to the
-        database correctly.
-      </h2>
-      <FormFields values={values} state={state} />
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          marginTop: 30,
-        }}
-      >
-        <SaveButton />
-        <DeleteButton
-          handleSubmit={handleSubmit}
-          setFieldValue={setFieldValue}
-        />
-      </div>
-      {process.env.NODE_ENV === "development" ? (
-        <pre>{JSON.stringify(values, null, 2)}</pre>
-      ) : null}
-    </Box>
-  </Form>
+  <DialogContent>
+    <Paper>
+      <Form onSubmit={handleSubmit}>
+        <Box
+          style={{ marginLeft: 10, display: "flex", flexDirection: "column" }}
+        >
+          <h2>
+            Warning: this interface under construction and may not write to the
+            database correctly.
+          </h2>
+          <FormFields values={values} state={state} />
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              marginTop: 30,
+            }}
+          >
+            <SaveButton />
+            <DeleteButton
+              handleSubmit={handleSubmit}
+              setFieldValue={setFieldValue}
+            />
+          </div>
+          {process.env.NODE_ENV === "development" ? (
+            <pre>{JSON.stringify(values, null, 2)}</pre>
+          ) : null}
+        </Box>
+      </Form>
+    </Paper>
+  </DialogContent>
 );
 
 export default ResourceForm;
