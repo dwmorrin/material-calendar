4c0ac3ae27e6eeb238480aa4881d2b3c4994d5c8
David Morrin <dm187@nyu.edu>
2021-07-20 09:40:19 -0400

    feat: shows project info in group record

--- src/admin/records/userGroup.record.ts
+++ src/admin/records/userGroup.record.ts
@@ -1,13 +1,27 @@
 import UserGroup, { GroupMember } from "../../resources/UserGroup";
+import Project from "../../resources/Project";
+import { AdminState } from "../types";
+import { ResourceKey } from "../../resources/types";
+import { parseAndFormatSQLDateInterval } from "../../utils/date";
 
 const nameReducer = (s: string, groupMember: GroupMember): string =>
   s ? `${s}, ${groupMember.username}` : groupMember.username;
 
-const template = (userGroup: unknown): string[][] =>
-  userGroup instanceof UserGroup
-    ? [
-        ["Project", userGroup.projectId.toString()],
-        ["Members", userGroup.members.reduce(nameReducer, "")],
-      ]
-    : [["", JSON.stringify(userGroup)]];
+const template = (userGroup: unknown, state: AdminState): string[][] => {
+  if (userGroup instanceof UserGroup) {
+    const projects = state.resources[ResourceKey.Projects] as Project[];
+    const project =
+      projects.find(({ id }) => id === userGroup.projectId) || new Project();
+    return [
+      ["ID", userGroup.id.toString()],
+      ["Title", userGroup.title],
+      ["Project", project.title],
+      ["Interval", parseAndFormatSQLDateInterval(project)],
+      ["Members", userGroup.members.reduce(nameReducer, "")],
+    ];
+  } else {
+    return [["", JSON.stringify(userGroup)]];
+  }
+};
+
 export default template;
