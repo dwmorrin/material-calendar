a22483806eab089ce662f0dc342c5cbe9bd280e1
David Morrin <dm187@nyu.edu>
2021-07-21 14:10:13 -0400

    fix: category record hides null parent ID

--- src/admin/records/category.record.ts
+++ src/admin/records/category.record.ts
@@ -1,12 +1,16 @@
 import Category from "../../resources/Category";
 
-const template = (category: unknown): string[][] =>
-  category instanceof Category
-    ? [
-        ["ID", category.id.toString()],
-        ["Title", category.title],
-        ["Parent ID", category.parentId?.toString() || "NULL"],
-      ]
-    : [["", JSON.stringify(category)]];
+const template = (category: unknown): string[][] => {
+  if (category instanceof Category) {
+    const record = [
+      ["ID", category.id.toString()],
+      ["Title", category.title],
+    ];
+    if (category.parentId)
+      // TODO: find parent title from state
+      record.push(["Parent ID", category.parentId.toString()]);
+    return record;
+  } else return [["", JSON.stringify(category)]];
+};
 
 export default template;
