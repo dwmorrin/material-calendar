38330459bf677a4899bd85b95ccd7a33d3181f31
David Morrin <dm187@nyu.edu>
2021-07-29 08:47:07 -0400

    fix: removes router props from Landing

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useEffect } from "react";
-import { RouteComponentProps, navigate, Router } from "@reach/router";
+import { navigate, Router } from "@reach/router";
 import { useAuth } from "./AuthProvider";
 import Calendar from "./Calendar";
 import Dashboard from "./admin/Dashboard";
@@ -7,12 +7,13 @@ import Dashboard from "./admin/Dashboard";
 /**
  * Redirect user to the appropriate page
  */
-const Landing: FC<RouteComponentProps> = () => {
+const Landing: FC = () => {
   const { isAdmin } = useAuth();
+
   useEffect(() => {
-    if (isAdmin) navigate("/admin");
-    else navigate("/calendar");
+    navigate(isAdmin ? "/admin" : "/calendar");
   }, [isAdmin]);
+
   return (
     <Router>
       <Calendar path="/calendar" />
