c799542d607fc45643f2e24d4d246201d7f22f27
David Morrin <dm187@nyu.edu>
2021-07-31 16:33:44 -0400

    fix: UserGroup has title

--- src/resources/UserGroup.ts
+++ src/resources/UserGroup.ts
@@ -17,26 +17,17 @@ interface UserGroup {
 }
 
 class UserGroup implements UserGroup {
-  static makeTitle(members: GroupMember[]): string {
-    if (members.length === 1) {
-      return members[0].name.first + " " + members[0].name.last;
-    }
-    const lastNames = members.map((member) => member.name.last);
-    if (lastNames.length < 3) {
-      return lastNames.join(" and ");
-    }
-    return lastNames.slice(0, -1).join(", ") + ", and " + lastNames.slice(-1);
-  }
   static url = "/api/groups";
   constructor(
     group = {
       id: 0,
       projectId: 0,
+      title: "",
       members: [] as GroupMember[],
+      reservedHours: 0,
     }
   ) {
     Object.assign(this, group);
-    this.title = UserGroup.makeTitle(this.members);
   }
 }
 
