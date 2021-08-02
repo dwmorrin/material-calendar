3e4e5b57bccc84bfda1964a831a398c45492a8e8
David Morrin <dm187@nyu.edu>
2021-08-02 11:28:23 -0400

    fix: partially adds user group interface

--- src/admin/forms/userGroup.values.ts
+++ src/admin/forms/userGroup.values.ts
@@ -1,15 +1,44 @@
+import { ResourceKey } from "../../resources/types";
+import User from "../../resources/User";
 import UserGroup from "../../resources/UserGroup";
 import { AdminState } from "../types";
 
+interface UserGroupValues extends Record<string, unknown> {
+  id: number;
+  projectId: string;
+  members: string[];
+  title: string;
+  reservedHours: number;
+}
+
 export const values = (state: AdminState): Record<string, unknown> => {
   const group = state.resourceInstance as UserGroup;
-  return { ...group };
+  return {
+    id: group.id,
+    projectId: String(group.projectId),
+    members: group.members.map(({ username }) => username),
+    title: group.title,
+    reservedHours: group.reservedHours,
+  } as UserGroupValues;
 };
 
 export const update = (
   state: AdminState,
   values: Record<string, unknown>
 ): UserGroup => {
-  const group = new UserGroup(state.resourceInstance as UserGroup);
-  return { ...group, ...values };
+  const { id, projectId, members, title, reservedHours } =
+    values as UserGroupValues;
+  const users = state.resources[ResourceKey.Users] as User[];
+  const _members = members.map((_username) => {
+    const { username, name, email } =
+      users.find((user) => user.username === _username) || new User();
+    return { username, name, email };
+  });
+  return {
+    id,
+    projectId: Number(projectId),
+    members: _members,
+    title,
+    reservedHours,
+  };
 };
--- src/components/admin/forms/UserGroup.tsx
+++ src/components/admin/forms/UserGroup.tsx
@@ -1,52 +1,77 @@
 import React, { FunctionComponent } from "react";
 import { Field, FieldArray } from "formik";
-import { TextField } from "formik-material-ui";
+import { TextField, RadioGroup } from "formik-material-ui";
 import { FormTemplateProps } from "../../../admin/types";
-import { List, FormLabel, Box, IconButton, Button } from "@material-ui/core";
+import {
+  List,
+  FormControlLabel,
+  FormLabel,
+  Box,
+  IconButton,
+  Button,
+  Radio,
+} from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 import DeleteIcon from "@material-ui/icons/Delete";
 import User from "../../../resources/User";
+import { ResourceKey } from "../../../resources/types";
+import Project from "../../../resources/Project";
 
-const FormTemplate: FunctionComponent<FormTemplateProps> = ({ values }) => (
-  <List>
-    <Field
-      fullWidth
-      component={TextField}
-      name="projectId"
-      label="Project ID"
-    />
-    <FormLabel>Group members</FormLabel>
-    <Box>
-      <FieldArray name="members">
-        {({ push, remove }): JSX.Element => (
-          <div>
-            {(values.members as User[]).map((_, index) => (
-              <div key={index}>
-                <Field
-                  component={TextField}
-                  name={`members[${index}].username`}
-                  label="Username"
-                />
-                <IconButton onClick={(): void => remove(index)}>
-                  <DeleteIcon />
-                </IconButton>
-              </div>
-            ))}
-            <Button
-              onClick={(): void => push("")}
-              size="small"
-              startIcon={<AddIcon />}
-              variant="contained"
-            >
-              Add group member
-            </Button>
-          </div>
-        )}
-      </FieldArray>
-    </Box>
-    <FormLabel>Reserved hours</FormLabel>
-    <p>{String(values.reservedHours)}</p>
-  </List>
-);
+const FormTemplate: FunctionComponent<FormTemplateProps> = ({
+  state,
+  values,
+}) => {
+  const projects = state.resources[ResourceKey.Projects] as Project[];
+  return (
+    <List>
+      <Field fullWidth component={TextField} name="title" label="Title" />
+      <FormLabel>Project</FormLabel>
+      <Field component={RadioGroup} name="projectId">
+        {projects.map(({ id, title }) => (
+          <FormControlLabel
+            key={id}
+            label={title}
+            value={String(id)}
+            control={<Radio />}
+          />
+        ))}
+      </Field>
+      <h3>
+        Warning: group membership editing disabled until logic check added
+      </h3>
+      <FormLabel>Group members</FormLabel>
+      <Box>
+        <FieldArray name="members">
+          {({ push, remove }): JSX.Element => (
+            <div>
+              {(values.members as User[]).map((_, index) => (
+                <div key={index}>
+                  <Field
+                    component={TextField}
+                    name={`members[${index}]`}
+                    label="Username"
+                  />
+                  <IconButton onClick={(): void => remove(index)}>
+                    <DeleteIcon />
+                  </IconButton>
+                </div>
+              ))}
+              <Button
+                onClick={(): void => push("")}
+                size="small"
+                startIcon={<AddIcon />}
+                variant="contained"
+              >
+                Add group member
+              </Button>
+            </div>
+          )}
+        </FieldArray>
+      </Box>
+      <FormLabel>Reserved hours</FormLabel>
+      <p>{String(values.reservedHours)}</p>
+    </List>
+  );
+};
 
 export default FormTemplate;
