01005b8920ca4fec43825cea064a3835ee582afc
David Morrin <dm187@nyu.edu>
2021-08-12 10:56:05 -0400

    fix: removes dead code

--- src/resources/Equipment.ts
+++ src/resources/Equipment.ts
@@ -1,7 +1,5 @@
 import Category from "./Category";
-import Event from "./Event";
 import Tag from "./Tag";
-import { parseSQLDatetime } from "../utils/date";
 
 interface Equipment {
   [k: string]: unknown;
@@ -27,47 +25,6 @@ interface Equipment {
 }
 
 class Equipment implements Equipment {
-  // returns the number of an item available during an event
-  static getNumberAvailable(item: Equipment, event: Event): number {
-    if (item.reservations.length === 0) {
-      return item.quantity;
-    }
-    const eventStartDate = parseSQLDatetime(event.start);
-    const eventEndDate = parseSQLDatetime(event.end);
-    const reserved = item.reservations
-      .filter(({ bookingId, start, end }) => {
-        const resStartDate = parseSQLDatetime(start);
-        const resEndDate = parseSQLDatetime(end);
-        return (
-          (event.reservation?.id !== bookingId &&
-            resStartDate <= eventStartDate &&
-            resEndDate > eventStartDate) ||
-          (resStartDate < eventEndDate && resStartDate >= eventStartDate)
-        );
-      })
-      .map((reservation) =>
-        // TODO: Remove this and figure out why the filter above is not working.
-        reservation.bookingId === event.reservation?.id
-          ? 0
-          : reservation.quantity
-      )
-      .reduce((a, b) => a + b, 0);
-    return item.quantity - reserved;
-  }
-
-  static getAvailableItems(items: Equipment[], event: Event): Equipment[] {
-    return items.reduce((available, item) => {
-      const numberAvailable = Equipment.getNumberAvailable(item, event);
-      if (numberAvailable > 0) {
-        available.push({
-          ...item,
-          quantity: numberAvailable,
-        });
-      }
-      return available;
-    }, [] as Equipment[]);
-  }
-
   static url = "/api/equipment";
   constructor(
     equip = {
