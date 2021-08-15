c1fc0fbcada9732955eb5038a28d5921eb80bdaa
David Morrin <dm187@nyu.edu>
2021-08-15 14:50:35 -0400

    fix: event import fixes leading zeros
    corrects yyyy-m-d h:m:s
    to yyyy-mm-dd hh:mm:ss
    
    todo: extend to reformat any input date format

--- src/admin/bulkImport/event.import.ts
+++ src/admin/bulkImport/event.import.ts
@@ -3,6 +3,21 @@ import Event from "../../resources/Event";
 import { BulkImporter } from "./router";
 import { ResourceKey } from "../../resources/types";
 
+// this can also be accomplished in mysql with DATE(input_string),
+// but easy enough to do it in javascript and doing it twice is fine as well
+// problem is if the spreadsheet formatted "2000-01-01" as "2000-1-1",
+// this will not compare correctly to existing dates without functions like
+// DATE().
+// TODO catch and re-format any date format, not just "yyyy-m-d"
+const fixLeadingZeros = (dateStr: string): string => {
+  const [date, time] = dateStr.split(" ");
+  const [year, month, day] = date.split("-");
+  const [hour, minute, second] = time.split(":");
+  const make2digit = (num: number): string => (num < 10 ? `0${num}` : `${num}`);
+  const res = [month, day, hour, minute, second].map(Number).map(make2digit);
+  return `${year}-${res[0]}-${res[1]} ${res[2]}:${res[3]}:${res[4]}`;
+};
+
 const headings = ["Title", "Location", "Start", "End", "Reservable"];
 
 const bulkImport: BulkImporter = (setSubmitting, dispatch, events) => {
@@ -23,8 +38,8 @@ const bulkImport: BulkImporter = (setSubmitting, dispatch, events) => {
       events.map(({ Title, Location, Start, End, Reservable }) => ({
         title: Title,
         locationId: Location,
-        start: Start,
-        end: End,
+        start: fixLeadingZeros(Start),
+        end: fixLeadingZeros(End),
         reservable: Number(Reservable),
       }))
     ),
