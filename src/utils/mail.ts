76d3dfebc619a2f2d61c5088128ef77abc2560c5
David Morrin <dm187@nyu.edu>
2021-08-16 14:53:00 -0400

    fix: removes dead code

--- src/utils/mail.ts
+++ src/utils/mail.ts
@@ -1,31 +1,3 @@
-export const sendMail = (
-  to: string,
-  subject: string,
-  text: string,
-  dispatchError: (error: Error, meta?: unknown) => void
-): void => {
-  fetch(`/api/mail`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      to: to,
-      subject: subject,
-      text: text,
-    }),
-  })
-    .then((response) => response.json())
-    .then(({ error, data }) => {
-      if (error || !data) return dispatchError(error);
-      // Success should be silent
-      /* dispatchError(
-        new Error("Email success, but no success handler written!")
-      ); */
-    })
-    .catch(dispatchError);
-};
-
 export interface Mail {
   to: string;
   subject: string;
