9894a6cd06c6aa52bb2e3359ede156abf0f3cca8
David Morrin <dm187@nyu.edu>
2022-01-18 13:27:44 -0500

    fix?: temporary hack to display instructor's courses

--- src/components/Project/ProjectList.tsx
+++ src/components/Project/ProjectList.tsx
@@ -7,26 +7,45 @@ import { ResourceKey } from "../../resources/types";
 import Project from "../../resources/Project";
 import Course from "../../resources/Course";
 import UserGroup from "../../resources/UserGroup";
+import { useAuth } from "../AuthProvider";
 
 type CourseProjects = Record<number, Project[]>;
 
 const ProjectList: FC<
   CalendarUIProps & CalendarUISelectionProps & { invitations: UserGroup[] }
 > = ({ dispatch, invitations, state, selections, setSelections }) => {
+  const { user } = useAuth();
+  const isInstructor = user.roles.includes("instructor");
   const projects = (state.resources[ResourceKey.Projects] as Project[]).filter(
     ({ title }) => title !== Project.walkInTitle
   );
   const courses = (state.resources[ResourceKey.Courses] as Course[]).filter(
     ({ title }) => title !== Project.walkInTitle
   );
-  const courseProjects = projects.reduce((acc, p) => {
-    const {
-      course: { id },
-    } = p;
-    if (!(id in acc)) acc[id] = [p];
-    else acc[id].push(p);
-    return acc;
-  }, {} as CourseProjects);
+
+  const classMeetingProject = projects.find((p) =>
+    p.title.startsWith(Project.classMeetingTitlePrefix)
+  );
+
+  let courseProjects: CourseProjects = {};
+
+  //! TEMPORARY HACK - needs to fix display of course/project to instructors
+  if (isInstructor) {
+    courseProjects = courses.reduce((acc, c) => {
+      if (classMeetingProject) acc[c.id] = [classMeetingProject];
+      return acc;
+    }, {} as CourseProjects);
+  } else {
+    courseProjects = projects.reduce((acc, p) => {
+      const {
+        course: { id },
+      } = p;
+      if (!(id in acc)) acc[id] = [p];
+      else acc[id].push(p);
+      return acc;
+    }, {} as CourseProjects);
+  }
+
   return (
     <div>
       {projects.length ? (
