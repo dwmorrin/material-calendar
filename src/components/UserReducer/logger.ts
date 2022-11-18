06e9a3575e46aae4df3d812b1ddced207a36a9c4
David Morrin <dm187@nyu.edu>
2022-11-18 12:01:17 -0500

    feat: reducer log shows before and after

--- src/components/UserReducer/logger.ts
+++ src/components/UserReducer/logger.ts
@@ -5,12 +5,20 @@ const logger: (r: StateHandler) => StateHandler = (reducer) => {
   if (process.env.NODE_ENV === "production") return reducer;
 
   const loggingReducer: StateHandler = (state, action) => {
+    const label = CalendarAction[action.type];
+    // eslint-disable-next-line no-console
+    console.group(label);
     // eslint-disable-next-line no-console
     console.log({
       state,
-      action: { ...action, type: CalendarAction[action.type] },
+      action: { ...action, type: label },
     });
-    return reducer(state, action);
+    const result = reducer(state, action);
+    // eslint-disable-next-line no-console
+    console.log({ finalState: result });
+    // eslint-disable-next-line no-console
+    console.groupEnd();
+    return result;
   };
   return loggingReducer;
 };
