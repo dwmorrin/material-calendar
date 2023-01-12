ea7e9358ec94cb7d643c5ba41fff6a9d76d51e6c
David Morrin <dm187@nyu.edu>
2023-01-12 10:54:55 -0500

    feat: adds extra try/catch level outside of React

--- src/index.js
+++ src/index.js
@@ -3,9 +3,25 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+try {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+} catch (e) {
+  // eslint-disable-next-line no-console
+  console.log(e);
+  // Clear the screen, display the message
+  let body = document.body;
+  if (!body) {
+    body = document.createElement("body");
+    document.append(body);
+  } else {
+    Array.from(body.children).forEach((child) => child.remove());
+  }
+  const heading = document.createElement("h1");
+  heading.textContent = "You are logged out. Try refreshing the page.";
+  body.append(heading);
+}
